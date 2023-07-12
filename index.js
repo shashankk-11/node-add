e8ba673e31db7f72d84e1d161829dea45ac2ee87
shashankk-11 <kulkarnishashank1100@gmail.com>
2023-07-12 23:26:21 +0530

    added name

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ function addNumbers(num1, num2) {
 
 const number1 = 5;
 const number2 = 10;
+var name = "samiksha"
 
 const result = addNumbers(number1, number2);
 console.log("The sum is:", result);
+console.log($name)
