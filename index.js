b64120aa9e556b9aa288b3d8c1400e40fcef8261
shashankk-11 <kulkarnishashank1100@gmail.com>
2023-07-12 23:18:13 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function addNumbers(num1, num2) {
+  return num1 + num2;
+}
+
+const number1 = 5;
+const number2 = 10;
+
+const result = addNumbers(number1, number2);
+console.log("The sum is:", result);
